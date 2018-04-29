f4d040b30382f433f71331804f542cc89baf2041
haitran <trannamhaibp@gmail.com>
2018-04-29 14:22:43 +0700

    Add project files.

---
+++ CSharpSection/Classes/BankAccount.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpSection.Classes
+{
+    public class BankAccount
+    {
+        //variable
+        private float balance;
+
+        //property
+        public float Balance
+        {
+            get
+            {
+                return balance;
+            }
+            private set
+            {
+                if (value >= 0)
+                {
+                    balance = value;
+                }
+                else
+                {
+                    balance = 0;
+                }
+            }
+        }
+
+        private string owner;
+
+        //overload contructor 
+        public BankAccount(float balance, string owner)
+        {
+            Balance = balance;
+            this.owner = owner;
+        }
+
+        //method
+        //virtual to able overriding
+        public virtual float AddBalance(float balanceToBeAdded)
+        {
+            Balance = balance + balanceToBeAdded;
+            return Balance;
+        }
+
+        //overload method : same name but differance signature
+        //virtual to able overriding
+        public virtual float AddBalance(float balanceToBeAdded, bool balanceCanBeNagative)
+        {
+            if (balanceCanBeNagative)
+            {
+                balance = balance + balanceToBeAdded;
+            }
+            else
+            {
+                Balance = balance + balanceToBeAdded;
+            }
+            return Balance;
+        }
+        
+        //Asynchronouns method
+        public async Task<string> GetData()
+        {
+            Thread.Sleep(2000);
+            return "Completed";
+        }
+    }
+
+    //Inheritance
+    public class ChildBankAccount : BankAccount
+    {
+        public string Parent { get; set; }
+        public ChildBankAccount(float balance, string owner, string parent) : base(balance, owner)
+        {
+            Parent = parent;
+        }
+
+        //override method had abled by virtual
+        public override float AddBalance(float balanceToBeAdded)
+        {
+            if (balanceToBeAdded >= -10)
+            {
+                return base.AddBalance(balanceToBeAdded);
+            }
+            else
+            {
+                return Balance;
+            }
+        }
+
+        //override method had abled by virtual
+        public override float AddBalance(float balanceToBeAdded, bool balanceCanBeNagative)
+        {
+            if (balanceToBeAdded >= -10)
+            {
+                return base.AddBalance(balanceToBeAdded, balanceCanBeNagative);
+            }
+            else
+            {
+                return Balance;
+            }
+        }
+    }
+}
---
+++ CSharpSection/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpSection.Classes;
+
+namespace CSharpSection
+{
+    class Program
+    {
+        static IOperations am;
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            BankAccount bankAccount1 = new BankAccount(1222.33f, "Haitn");
+            Console.WriteLine(bankAccount1.Balance);
+
+            BankAccount bankAccount2 = new BankAccount(1333.33f, "tnHai");
+
+            ChildBankAccount bankAccount3 = new ChildBankAccount(1333.33f, "tnHai", "Haitn");
+            Console.WriteLine(bankAccount1.AddBalance(1000f));
+            Console.WriteLine(bankAccount2.AddBalance(-2000f));
+            Console.WriteLine(bankAccount2.AddBalance(-2000f, true));
+            Console.WriteLine(bankAccount3.Parent);
+            
+            am = new AdvancedMath();
+            Console.WriteLine(am.Remainder(7, 3));
+
+            //using asynchronouns method
+            GetData();
+            
+            Console.ReadLine();
+        }
+
+        
+        //call asynchoronous method
+        async static void GetData()
+        {
+            BankAccount bankAccount = new BankAccount(5000f,"HaiTread");
+            Console.WriteLine("Login");
+            var task = await bankAccount.GetData();
+            Console.WriteLine(task);
+        }
+        
+        interface IOperations
+        {
+            float Remainder(float dividend, float divisor);
+        }
+
+        class SimpleMath
+        {
+            public static float Add(float n1, float n2)
+            {
+                return n1 + n2;
+            }
+        }
+
+        //inheritance and implementing interface
+        class AdvancedMath : SimpleMath, IOperations
+        {
+            public float Remainder(float dividend, float divisor)
+            {
+                return dividend % divisor; 
+            }
+        }
+    }
+}
